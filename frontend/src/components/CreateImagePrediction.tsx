076d0bd763139f5d2e2aea8a22a529c001ee8564
Vikraman Senthil Kumar <93839060+Vikramansen@users.noreply.github.com>
2023-06-03 17:16:53 -0700

    Delete frontend directory

--- frontend/src/components/CreateImagePrediction.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactElement } from "react";
-
-const CreateImagePrediction = (): ReactElement => {
-  return <div>Image Classification</div>;
-};
-
-export default CreateImagePrediction;
--- frontend/src/components/NotFound.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactElement } from "react";
-
-const NotFound = (): ReactElement => {
-  return <div> Page Not Found </div>;
-};
-
-export default NotFound;
