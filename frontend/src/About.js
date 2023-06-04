434d41111bafefc46cd8a735ec616539ad367a40
Vikraman Senthil Kumar <93839060+Vikramansen@users.noreply.github.com>
2023-06-03 17:20:37 -0700

    Add files via upload

---
+++ frontend/src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Write something here, kaustabh</p> 
+    </div>
+  );
+}
+
+export default About;
