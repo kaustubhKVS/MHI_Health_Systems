fffe0199c255eee092eb9fda4cf499b32e3144a5
Kaustubh Shete <kvshete15@gmail.com>
2025-07-24 16:14:12 -0400

    Update config_aws.py
    Security Fixes

--- backend/config_aws.py
+++ backend/config_aws.py
@@ -4,7 +4,7 @@
 
 
 # You accidently put comma in some setting, then the variable was recognised as a tuple insted of a string
-aws_account_access_key = "AKIASNVQFMIOQQID5AGA"
-aws_account_secret_access_key = "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"
-aws_region_name = "us-east-1"
-aws_s3_bucket_name = 'images-mhi'
+aws_account_access_key = ""
+aws_account_secret_access_key = "
+aws_region_name = ""
+aws_s3_bucket_name = ""
